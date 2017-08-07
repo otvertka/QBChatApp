d59e643a06ee81106fe662e9117ea321010f7ce6
otvertka <mazilla_D@tut.by>
2017-08-07 23:08:53 +0300

    Login and SignUp

--- app/src/main/java/com/example/qbchatapp/MainActivity.java
+++ app/src/main/java/com/example/qbchatapp/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.qbchatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.quickblox.auth.session.QBSettings;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String APP_ID = "61125";
+    static final String AUTH_KEY = "Amm8bhkt5QGpOQf";
+    static final String AUTH_SECRET = "3L6gmE3vYA9cU3G";
+    static final String ACCOUNT_KEY = "hTBL4FKVA-GzWZxGq1Y6";
+
+    Button btnLogin, btnSignUp;
+    EditText edtUser, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeFramework();
+
+        btnLogin = (Button)findViewById(R.id.main_btnLogin);
+        btnSignUp = (Button)findViewById(R.id.main_btnSignUp);
+
+        edtUser = (EditText)findViewById(R.id.main_editLogin);
+        edtPassword = (EditText)findViewById(R.id.main_editPassword);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = edtUser.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                QBUser qbUser = new QBUser(user, password);
+
+                QBUsers.signIn(qbUser).performAsync(new QBEntityCallback<QBUser>() {
+                    @Override
+                    public void onSuccess(QBUser qbUser, Bundle bundle) {
+                        Toast.makeText(getBaseContext(), "Login successfully", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(QBResponseException e) {
+                        Toast.makeText(getBaseContext(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void initializeFramework() {
+        QBSettings.getInstance().init(getApplicationContext(), APP_ID, AUTH_KEY, AUTH_SECRET);
+        QBSettings.getInstance().setAccountKey(ACCOUNT_KEY);
     }
 }
